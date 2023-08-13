de0bba513bdeafee983e5014fdf378a0d948887c
Hengki Agung Prayoga <xeon699@gmail.com>
2023-08-13 13:51:29 +0800

    make layout app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,53 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+@extends("layouts.app")
 
-@include('layouts.head')
-
-<body>
-    <div>
-        <!-- #Main ============================ -->
-        <div class="page-container">
-            <!-- ### $Topbar ### -->
-            @include('layouts.partials.topbar')
-
-            <!-- ### $App Screen Content ### -->
-            <main class='main-content bgc-grey-100'>
-                <div id='mainContent'>
-                    <div class="container-fluid">
-
-                        <h4 class="c-grey-900 mT-10 mB-30">@yield('page-header')</h4>
-
-                        @include('layouts.partials.messages')
-
-
-                    </div>
-                </div>
-            </main>
-
-            <!-- ### $App Screen Footer ### -->
-            <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
-                <span>Copyright © {{ date('Y') }} Designed by
-                    <a>Colorlib</a>. Created By Patrick Polii All rights
-                    reserved.</span>
-            </footer>
-        </div>
-
-        <!-- #Left Sidebar ==================== -->
-        @include('layouts.partials.sidebar')
-
-    </div>
-
-    {{-- @yield('script')
-    <script src="{{ mix('/js/app.js') }}"></script>
-
-    <!-- Global js content -->
-
-    <!-- End of global js content-->
-
-    <!-- Specific js content placeholder -->
-    @stack('js')
-    <!-- End of specific js content placeholder --> --}}
-
-</body>
-
-</html>
+@section("content")
+asdfasdf
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-</head>
-<body>
-    @yield('content')
+<html lang="{{ app()->getLocale() }}">
 
-    <script src="{{ asset('js/app.js') }}"></script>
+@include('layouts.head')
+
+<body>
+    <div>
+        <!-- #Main ============================ -->
+        <div class="page-container">
+            <!-- ### $Topbar ### -->
+            @include('layouts.partials.topbar')
+
+            <!-- ### $App Screen Content ### -->
+            <main class='main-content bgc-grey-100'>
+                <div id='mainContent'>
+                    <div class="container-fluid">
+
+                        <h4 class="c-grey-900 mT-10 mB-30">@yield('page-header')</h4>
+
+                        @include('layouts.partials.messages')
+
+                        @yield('content')
+
+                    </div>
+                </div>
+            </main>
+            <!-- ### $App Screen Footer ### -->
+            <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600">
+                <span>Copyright © {{ date('Y') }} Designed by
+                    <a>Colorlib</a>. Created By Patrick Polii All rights
+                    reserved.</span>
+            </footer>
+        </div>
+
+        <!-- #Left Sidebar ==================== -->
+        @include('layouts.partials.sidebar')
+
+    </div>
+
+    <script src="{{ asset('/js/app.js') }}"></script>
+
+    <!-- Global js content -->
+
+    <!-- End of global js content-->
+
+    <!-- Specific js content placeholder -->
+    @stack('js')
+    <!-- End of specific js content placeholder --> --}}
+
 </body>
+
 </html>
