f6fa701a232d15e029edc13614dca05329066c2d
Richard Owen <richardowen2411@gmail.com>
2024-02-29 22:28:09 +0800

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,38 +1,63 @@
-@extends('layouts.auth.app')
+<!doctype html>
+<html>
 
-@section('content')
-<h4 class="fw-300 c-grey-900 mB-40 py-5">Login</h4>
-<form class="form-horizontal" method="POST" action="{{ route('login') }}">
-    {{ csrf_field() }}
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
 
-    <div class="form-group mb-5 {{ $errors->has('email') ? ' has-error' : '' }}">
-        <label for="email" class="text-dark form-label">Email</label>
-        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+<body>
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 bg-gray-100">
+        <div class="w-full grid grid-cols-2 shadow md:mt-0 sm:max-w-4xl min-h-[65vh]">
+            <div class="col-span-1 bg-white rounded-l-2xl p-4 flex items-center justify-center w-full p-8">
+                <form method="POST" class="w-full" action="{{ route('login') }}">
+                    <div class="flex flex-1 justify-center mb-6">
 
-        @if ($errors->has('email'))
-            <span class="form-text text-danger">
-                <small>{{ $errors->first('email') }}</small>
-            </span>
-        @endif
-    </div>
+                        <img src="{{ asset('images/logo.png') }}" class="h-12" alt="">
+                    </div>
+                    {{ csrf_field() }}
+                    <div for="email" class="mb-1 text-sm font-semibold">Email</div>
+                    <input id="email" type="email" name="email"
+                        class="w-full rounded-lg border border-gray-300 px-1 py-1 focus:ring-3 focus:ring-primary-300 focus:border-primary-300"
+                        value="{{ old('email') }}" required autofocus>
+                    @if ($errors->has('email'))
+                        <span class="text-red-600">
+                            <small>{{ $errors->first('email') }}</small>
+                        </span>
+                    @endif
 
-    <div class="form-group mb-5 {{ $errors->has('password') ? ' has-error' : '' }}">
-        <label for="password" class="text-dark form-labe">Password</label>
-        <input id="password" type="password" class="form-control" name="password" required>
+                    <div for="password" class="mb-1 text-sm font-semibold">Password</div>
+                    <input id="password" type="password"
+                        class="w-full rounded-lg border border-gray-300 px-1 py-1 focus:ring-3 focus:ring-primary-300 focus:border-primary-300"
+                        name="password" required>
+                    @if ($errors->has('password'))
+                        <span class="text-red-600">
+                            <small>{{ $errors->first('password') }}</small>
+                        </span>
+                    @endif
 
-        @if ($errors->has('password'))
-            <span class="form-text text-danger">
-                <small>{{ $errors->first('password') }}</small>
-            </span>
-        @endif
-    </div>
-
-    <div class="form-group">
-        <div class="peers ai-c jc-sb fxw-nw">
-            <div class="peer">
-                <button class="btn btn-primary">Login</button>
+                    <div class="flex flex-1 justify-center mt-6">
+                        <button type="submit"
+                            class="bg-blue-800 rounded-xl px-20 py-2 text-white font-semibold">Login</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-span-1 bg-blue-800 rounded-r-2xl p-8 flex justify-center items-center">
+                <div class="font-bold text-white p-4">
+                    <p class="text-3xl mb-4">Selamat Datang di Sisukma ITK</p>
+                    <p class="mb-4 font-semibold">Apabila anda mengalami kesulitan dalam login, silahkan hubungi Admin
+                        SIKUSMA lewat email:eoffice@itk.ac.id</p>
+                    {{-- <p class="mb-4">
+                        Apabila anda belum memiliki akun, silahkan hubungi Admin SIKUSMA lewat email:eoffice@itk.ac.id untuk mendapatkan akun
+                    </p> --}}
+                </div>
             </div>
         </div>
     </div>
-</form>
-@endsection
+    </section>
+
+</body>
+
+</html>
